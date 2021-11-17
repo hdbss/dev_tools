ce55acf49e68a789046700294c504264b0544435
hushuai <2858824818@qq.com>
2021-11-17 22:36:27 +0800

    done!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dev_tools
 这是一个娱乐项目
+
+#pdfto_img.py
+这是一个pdf转换图片的可视化脚本
--- pdfto_img.py
+++ pdfto_img.py
@@ -4,6 +4,11 @@ import tkinter
 
 
 def pyMuPDF_fitz(pdfPath, imagePath, img_type='jpg'):
+    """
+    :param pdfPath: pdf绝对路径
+    :param imagePath: 转化为图片的目录-绝对路径
+    :param img_type: 图片类型，默认jpg
+    """
     pdfDoc = fitz.open(pdfPath)
     for pg in range(pdfDoc.pageCount):
         page = pdfDoc[pg]
@@ -21,55 +26,59 @@ def pyMuPDF_fitz(pdfPath, imagePath, img_type='jpg'):
         pix.save(imagePath + '/' + 'images_%s.%s' % (pg, img_type))  # 将图片写入指定的文件夹内
 
 
+def covert_format(pdfPath, imagePath, img_format, text3):
+    try:
+        pyMuPDF_fitz(pdfPath, imagePath, img_format)
+        text3.delete(1.0, tkinter.END)
+        text3.insert('insert', '成功')
+        print('成功')
+    except:
+        print('失败，请重新再来')
+        text3.delete(1.0, tkinter.END)
+        text3.insert('insert', '失败，请重新再来')
+
 def PdfGui():
     window = tkinter.Tk()
     window.title('Pdf转图片')
     window.geometry('700x400')
+
+    # 单选框框架
     frame_radio = tkinter.Frame(window, height=6)
-    frame_radio.grid(row=5, column=1, ipadx=50, ipady=10, padx=20)
-    img_type = tkinter.IntVar()
+    frame_radio.grid(row=5, column=0, ipadx=50, ipady=10, padx=20)
+    img_type = tkinter.StringVar()
     img_type.set('jpg')
     img_types = [('jpg', 'jpg'), ('png', 'png'), ('jpeg', 'jpeg'), ('tiff', 'tiff'), ('gif', 'gif')]
     for text, value in img_types:
         radio = tkinter.Radiobutton(frame_radio, text=text, value=value, variable=img_type)
-        print(img_types.index((text, value)))
         column = img_types.index((text, value))
         radio.grid(row=0, column=column)
-    label1 = tkinter.Label(window, text='输入pdf路径:')
-    label2 = tkinter.Label(window, text='输入img路径:')
-    label1.grid(row=7, column=0)
-    label2.grid(row=8, column=0)
-    text1 = tkinter.Entry(window, show=None, width=40, borderwidth=2)
-    text2 = tkinter.Entry(window, show=None, width=40, borderwidth=2)
-    text1.grid(row=7, column=1, pady=2)
-    text2.grid(row=8, column=1)
-    button1 = tkinter.Button(window, text='确定', command=lambda: pyMuPDF_fitz(text1.get(), text2.get()))
+    # 单选框结束
+
+    # 输入框框架
+    frame_label = tkinter.Frame(window)
+    frame_label.grid(row=7, column=0, ipadx=50, ipady=10, padx=20)
+    label1 = tkinter.Label(frame_label, text='输入pdf路径:', font=(None, 16))
+    label2 = tkinter.Label(frame_label, text='输入img路径:', font=(None, 16))
+    label1.grid(row=7, column=0, sticky=tkinter.SE)
+    label2.grid(row=8, column=0, sticky=tkinter.SE)
+    text1 = tkinter.Entry(frame_label, show=None, width=55, borderwidth=2, relief=tkinter.GROOVE, bd=2)
+    # bd边框  relief边框格式  width框长度
+    text2 = tkinter.Entry(frame_label, show=None, width=55, borderwidth=2, relief=tkinter.GROOVE, bd=2)
+    text1.grid(row=7, column=1, pady=2, ipadx=3, ipady=2)
+    # row，column行列  padx，pady框外样式内  ipadx，ipady框内
+    text2.grid(row=8, column=1, pady=2, ipadx=3, ipady=2)
+    button1 = tkinter.Button(frame_label, text='确定', font=(None, 19),
+                             command=lambda: covert_format(text1.get(), text2.get(), img_type.get(), text3))
     button1.grid(column=1)
-    text3 = tkinter.Text(window)
-    text3.grid(row=10)
-    text3.insert('insert', '123')
+    # 输入框结束
+
+    text3 = tkinter.Text(window, height=2)
+    text3.grid()
     window.mainloop()
+
 
 PdfGui()
 
-
-# if __name__ == "__main__":
-#     type_all = ['jpeg', 'jpg', 'tiff', 'gif', 'png', '']
-#     img_type = input('输入图片类型(jpg,png?):')
-#     while img_type not in type_all:
-#         print('图片格式不存在,需重新输入')
-#         img_type = input('输入图片类型(jpg,png?):')
-#     while True:
-#         pdfpath = input('输入pdf路径:')
-#         imgpath = input('输入img路径:')
-#         if not os.path.exists(pdfpath):
-#             print('不存在pdf路径:{}'.format(pdfpath))
-#             continue
-#         elif not os.path.exists(imgpath):
-#             print('不存在此图片路径:{}，需手动创建'.format(imgpath))
-#         else:
-#             pyMuPDF_fitz(pdfpath, imgpath, img_type)
-#             print('成功')
 
 # C:\Users\admin\Desktop\标定图片\pdf\个人独资企业变更登记1\11310118342309462Y331010071700402_496_tpl_1636529634992.pdf
 # C:\Users\admin\Desktop\标定图片\test_covfpdf
